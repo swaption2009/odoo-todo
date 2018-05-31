6e6e1eeda19c78f5758b27f97c1dc9d10a42ac75
swaption2009 <swaption2009@gmail.com>
2018-05-31 12:43:11 -0700

    add res.partner many2many model and todo menu and basic form

--- custom-addons/todo_app/__manifest__.py
+++ custom-addons/todo_app/__manifest__.py
@@ -3,5 +3,8 @@
     'description': 'Manage personal to-do tasks.',
     'author': 'Daniel Reis',
     'depends': ['base'],
+    'data': ['views/todo_menu.xml',
+             'views/todo_view.xml'
+             ],
     'application': True,
 }
